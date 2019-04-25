4e824c156d26614bda680d988ea37f24681bc783
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-04-25 11:39:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get('/project/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
